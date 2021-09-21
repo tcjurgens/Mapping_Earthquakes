7423729d0738056eacd5f8d45708bec23c1a987a
tjurgens22 <72570333+tjurgens22@users.noreply.github.com>
2021-09-20 22:22:01 -0400

    /

--- Simple_Map/logic.js
+++ Simple_Map/logic.js
@@ -11,6 +11,9 @@ let map = L.map("mapid", {
 
 //now, adding a title layer for our map
 // We create the tile layer that will be the background of our map.
+
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
